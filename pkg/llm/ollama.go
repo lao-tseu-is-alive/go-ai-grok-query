038449d2b2eb46b0020e61fec35acc6a4b5ccfd3
cgil <lao.tseu.is.alive@gmail.com>
2025-09-09 13:52:19 +0200

    corrected bug in list for ollama provider

--- pkg/llm/ollama.go
+++ pkg/llm/ollama.go
@@ -154,11 +154,13 @@ func (o *OllamaProvider) ListModels(ctx context.Context) ([]ModelInfo, error) {
 
 	// remove from our list the embedding
 
-	modelInfos := make([]ModelInfo, len(resp.Models))
-	for i, model := range resp.Models {
+	modelInfos := make([]ModelInfo, 0, len(resp.Models))
+	for _, model := range resp.Models {
 		// o.l.Info("ollama model info %s: %#v", model.Name, model)
-		if !strings.Contains(model.Name, "embed") && !strings.Contains(model.Name, "paraphrase") {
-			modelInfos[i] = ModelInfo{
+		if !strings.Contains(model.Name, "embed") &&
+			!strings.Contains(model.Name, "paraphrase") &&
+			!strings.Contains(model.Name, "all-minilm") {
+			tempModelInfo := ModelInfo{
 				Name:               model.Name,
 				Family:             model.Details.Family,
 				Size:               model.Size,
@@ -167,95 +169,98 @@ func (o *OllamaProvider) ListModels(ctx context.Context) ([]ModelInfo, error) {
 				SupportsTools:      false,
 				SupportsThinking:   false,
 				SupportsInputImage: false,
-				SupportsStreaming:  false,
+				SupportsStreaming:  true,
 				SupportsJSONMode:   false,
 				SupportsStructured: false,
 			}
 			// adjust context size and other specifics based on documentation
 			switch model.Name {
 			case "deepcoder:1.5b", "deepcoder:latest":
-				modelInfos[i].ContextSize = 131072
+				tempModelInfo.ContextSize = 131072
 			case "deepseek-r1:latest", "deepseek-r1:1.5b", "deepseek-r1:7b,", "deepseek-r1:8b", "deepseek-r1:14b", "deepseek-r1:32b", "deepseek-r1:70b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
-				modelInfos[i].SupportsThinking = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
+				tempModelInfo.SupportsThinking = true
 			case "devstral:latest", "devstral:24b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
 			case "dolphin3:latest", "dolphin3:8b":
-				modelInfos[i].ContextSize = 131072
+				tempModelInfo.ContextSize = 131072
 			case "exaone-deep:latest", "exaone-deep:2.4b", "exaone-deep:7.8b", "exaone-deep:32b":
-				modelInfos[i].ContextSize = 32768
+				tempModelInfo.ContextSize = 32768
 			case "falcon3:latest", "falcon3:3b", "falcon3:7b", "falcon3:10b":
-				modelInfos[i].ContextSize = 32768
+				tempModelInfo.ContextSize = 32768
 			case "gemma3:270m", "gemma3:1b":
-				modelInfos[i].ContextSize = 32768
+				tempModelInfo.ContextSize = 32768
 			case "gemma3:latest", "gemma3:4b", "gemma3:12b", "gemma3:27b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsInputImage = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsInputImage = true
 			case "gpt-oss:latest", "gpt-oss:20b", "gpt-oss:120b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
-				modelInfos[i].SupportsThinking = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
+				tempModelInfo.SupportsThinking = true
 			case "llama3.1:8b-instruct-q8_0", "llama3.1:latest", "llama3.1:8b", "llama3.1:70b", "llama3.1:405b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
 			case "llama3.2:latest", "llama3.2:1b", "llama3.2:3b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
 			case "llama3.2-vision:latest", "llama3.2-vision:11b", "llama3.2-vision:90b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsInputImage = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsInputImage = true
 			case "llava:latest", "llava:7b":
-				modelInfos[i].ContextSize = 32768
-				modelInfos[i].SupportsInputImage = true
+				tempModelInfo.ContextSize = 32768
+				tempModelInfo.SupportsInputImage = true
 			case "llava:13b", "llava:34b":
-				modelInfos[i].ContextSize = 4096
-				modelInfos[i].SupportsInputImage = true
+				tempModelInfo.ContextSize = 4096
+				tempModelInfo.SupportsInputImage = true
 			case "magistral:latest", "magistral:24b":
-				modelInfos[i].ContextSize = 40000
-				modelInfos[i].SupportsTools = true
-				modelInfos[i].SupportsThinking = true
+				tempModelInfo.ContextSize = 40000
+				tempModelInfo.SupportsTools = true
+				tempModelInfo.SupportsThinking = true
 			case "mistral-nemo:latest", "mistral-nemo:12b":
-				modelInfos[i].ContextSize = 1024000
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 1024000
+				tempModelInfo.SupportsTools = true
 			case "mistral-small3.1:latest", "mistral-small3.1:24b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
-				modelInfos[i].SupportsInputImage = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
+				tempModelInfo.SupportsInputImage = true
 			case "mistral-small3.2:latest", "mistral-small3.2:24b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
-				modelInfos[i].SupportsInputImage = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
+				tempModelInfo.SupportsInputImage = true
 			case "mistral-small:latest", "mistral-small:24b":
-				modelInfos[i].ContextSize = 32768
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 32768
+				tempModelInfo.SupportsTools = true
 			case "mistral-small:22b":
-				modelInfos[i].ContextSize = 131072
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 131072
+				tempModelInfo.SupportsTools = true
 			case "mistral:latest", "mistral:7b":
-				modelInfos[i].ContextSize = 32768
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 32768
+				tempModelInfo.SupportsTools = true
 			case "mixtral:latest", "mixtral:8x7b":
-				modelInfos[i].ContextSize = 32768
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 32768
+				tempModelInfo.SupportsTools = true
 			case "mixtral:8x22b":
-				modelInfos[i].ContextSize = 65536
-				modelInfos[i].SupportsTools = true
+				tempModelInfo.ContextSize = 65536
+				tempModelInfo.SupportsTools = true
 			case "openthinker:latest", "openthinker:7b", "openthinker:32b":
-				modelInfos[i].ContextSize = 32768
+				tempModelInfo.ContextSize = 32768
 			case "phi4:latest", "phi4:14b":
-				modelInfos[i].ContextSize = 16384
+				tempModelInfo.ContextSize = 16384
 			case "qwen3:latest", "qwen3:0.6b", "qwen3:1.7b", "qwen3:8b", "qwen3:14b", "qwen3:32b":
-				modelInfos[i].ContextSize = 40960
-				modelInfos[i].SupportsTools = true
-				modelInfos[i].SupportsThinking = true
+				tempModelInfo.ContextSize = 40960
+				tempModelInfo.SupportsTools = true
+				tempModelInfo.SupportsThinking = true
 			case "qwen3-coder:latest", "qwen3-coder:30b", "qwen3-coder:480b":
-				modelInfos[i].ContextSize = 262144
+				tempModelInfo.ContextSize = 262144
 			case "qwen3:4b", "qwen3:30b", "qwen3:235b":
-				modelInfos[i].ContextSize = 262144
-				modelInfos[i].SupportsTools = true
-				modelInfos[i].SupportsThinking = true
+				tempModelInfo.ContextSize = 262144
+				tempModelInfo.SupportsTools = true
+				tempModelInfo.SupportsThinking = true
+			}
+			if tempModelInfo.Name != "" {
+				modelInfos = append(modelInfos, tempModelInfo)
 			}
 		} else {
 			o.l.Warn("ollama model embedding %s discarded: %#v", model.Name, model)
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goAiQuery"
 	AppSnake   = "go-ai-llm-query"
-	VERSION    = "0.2.5"
+	VERSION    = "0.2.6"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-ai-llm-query"
 	REVISION   = ""
 	BuildStamp = ""
