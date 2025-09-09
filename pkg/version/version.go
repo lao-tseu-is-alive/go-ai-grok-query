89c0d776c5a79f026be9f58ce1a95ba876289a7c
cgil <lao.tseu.is.alive@gmail.com>
2025-09-09 17:02:21 +0200

    added release build for askToAllModel

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goAiQuery"
 	AppSnake   = "go-ai-llm-query"
-	VERSION    = "0.2.7"
+	VERSION    = "0.2.8"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-ai-llm-query"
 	REVISION   = ""
 	BuildStamp = ""
